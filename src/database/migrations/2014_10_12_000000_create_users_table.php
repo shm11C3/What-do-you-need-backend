de5809b1f32ea9cbaf5a0d8a5580c0b16333e9fc
shm <s.murakami.tech@gmail.com>
2022-06-01 17:55:35 +0000

    Change: `users`に`profile_img_uri`を追加、カラム名と型の変更

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->string('auth_id', 62)->primary();      // Auth0から取得した`user_id`
-            $table->string('name', 45);                    // ユーザが登録する名前
-            $table->string('username', 16)->unique();      // @usernameで表示される一意の文字列
-            $table->unsignedSmallInteger('country_code');  // ISO 3166-1に基づく国コード（3桁）
+            $table->string('auth_id', 62)->primary();           // Auth0から取得した`user_id`
+            $table->string('name', 45);                         // ユーザが登録する名前
+            $table->string('username', 16)->unique();           // @usernameで表示される一意の文字列
+            $table->unsignedSmallInteger('country_id');         // Countryモデルを参照
+            $table->string('profile_img_uri', 255)->nullable(); // プロフィール画像を配信しているURI
             $table->boolean('delete_flg')->default(false);
             $table->timestamps();
         });
